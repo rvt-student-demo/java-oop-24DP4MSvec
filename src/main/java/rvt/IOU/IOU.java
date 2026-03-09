600b41c4706aad73f6a20772c77ff1263a137a4f
Mihails Shvecovs <A240932MS@rvt.lv>
2026-03-09 13:55:29 +0200

    I owe you completed

---
+++ src/main/java/rvt/IOU/IOU.java
@@ -0,0 +1,20 @@
+package rvt.IOU;
+
+import java.util.HashMap;
+
+public class IOU {
+    private HashMap<String, Double> debts;
+
+   public IOU() {
+        this.debts = new HashMap<>();
+    }
+    
+    public void setSum(String toWhom, double amount){
+        this.debts.put(toWhom, amount);
+
+}
+    public double howMuchDoIOweTo(String toWhom){
+        return this.debts.getOrDefault(toWhom, 0.0);
+    }
+}
+
