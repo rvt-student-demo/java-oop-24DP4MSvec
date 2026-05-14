7b0b9a40e6e9fc3b8c031ba81528744ac4dd476b
Mihails Shvecovs <A240932MS@rvt.lv>
2026-05-14 11:00:16 +0300

    Java graphic started.

---
+++ src/main/java/rvt/Graphic/JavaGrafika.java
@@ -0,0 +1,15 @@
+package rvt.Graphic;
+
+import javax.swing.JFrame;
+
+public class JavaGrafika {
+    public static void main(String[] args) {
+        
+        JFrame frame = new JFrame("Java grafika");
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        frame.setSize(200, 200);
+        frame.setVisible(true);
+    }
+}
